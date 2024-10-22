16469bb0c9a6c56a161867c733095f9ed5a97e50
Piquerez09 <joachim.andradeimbru@gmail.com>
2024-10-22 18:57:50 -0300

    jcc

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
 // Por enquanto, não temos funcionalidades específicas, mas aqui podemos adicionar interações no futuro
 console.log("Bem-vindo ao Marvel Fans!");
+<script>
+    const toggleButton = document.getElementById('theme-toggle');
+
+    toggleButton.addEventListener('click', () => {
+        document.body.classList.toggle('light');
+        document.querySelector('header').classList.toggle('light');
+        document.querySelectorAll('h2').forEach(h2 => h2.classList.toggle('light'));
+        document.querySelector('footer').classList.toggle('light');
+        toggleButton.classList.toggle('light');
+    });
+</script>
